b171b3fc415d7def518d28ad38f799aea5b53bde
Peyas Barmon <dev.peyasbarmon0024@gmail.com>
2026-04-04 17:11:55 +0600

    Private Route Added

---
+++ src/routes/PrivateRoute.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import { Navigate, useLocation } from "react-router";
+
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+
+  const location = useLocation();
+
+  if (loading) return <p>Loading....</p>;
+
+  if (user) return children;
+
+  return <Navigate to={"/login"} state={location.pathname}></Navigate>;
+};
+
+export default PrivateRoute;
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -3,12 +3,38 @@ import { createBrowserRouter } from "react-router";
 import RootLayout from "../layouts/RootLayout";
 import Login from "../pages/Login";
 import Signup from "../pages/Signup";
+import Home from "../pages/Home";
+import AllProperties from "../pages/AllProperties";
+import AddProperty from "../pages/AddProperty";
+import MyProperties from "../pages/MyProperties";
+import MyRatings from "../pages/MyRatings";
+import PrivateRoute from "./PrivateRoute";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <RootLayout></RootLayout>,
         children: [
+            {
+                index: true,
+                element: <Home></Home>
+            },
+            {
+                path: 'all-properties',
+                element: <AllProperties></AllProperties>
+            },
+            {
+                path: 'add-property',
+                element: <PrivateRoute><AddProperty></AddProperty></PrivateRoute>
+            },
+            {
+                path: 'my-properties',
+                element: <PrivateRoute><MyProperties></MyProperties></PrivateRoute>
+            },
+            {
+                path: 'my-ratings',
+                element: <PrivateRoute><MyRatings></MyRatings></PrivateRoute>
+            },
             {
                 path: '/login',
                 element: <Login></Login>
